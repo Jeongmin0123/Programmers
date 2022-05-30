6a79a5c543cc6243ce2f44d2c0e7064d8a5ab74a
Jeongmin Jang <xvsgxv@gmail.com>
2022-05-30 21:10:50 +0900

    JAVA_Level2_카펫

--- Programmers/Java/Level2/Carpet.java
+++ Programmers/Java/Level2/Carpet.java
@@ -3,6 +3,7 @@ package Level2;
 class Carpet {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
+        
         for(int h = 3 ; h*h <= (brown+yellow) ; h++) {
             if((brown+yellow) % h == 0) {
                 int w = (brown+yellow) / h;
