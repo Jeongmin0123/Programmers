bed58bfd257c66c423a0f3f7b7723a3072922a98
Jeongmin Jang <xvsgxv@gmail.com>
2022-05-31 22:47:19 +0900

    JAVA_Level2_전화번호 목록

---
+++ Programmers/Java/Level2/Phone_List.java
@@ -0,0 +1,13 @@
+package Level2;
+
+import java.util.*;
+
+class Phone_List {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++)
+            if (phone_book[i + 1].startsWith(phone_book[i]))
+                return false;
+        return true;
+    }
+}
